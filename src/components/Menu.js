6ac5de413fb11af2ca1fd6b1e9dec29559a1fc48
vasilisa <igorevna.vasilisa@gmail.com>
2017-08-24 02:16:33 +0300

    up

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -21,49 +21,51 @@ class Menu extends Component {
   render() {
     return (
       <div className={'menu ' + this.props.type} onClick={this.handleClick.bind(this)}>
-        <span className='menu-link-wrap'>
-          <Link to="/principles" currentPage={this.props.currentPage} className='menu-link'>Principles</Link>
-        </span>
-        <span className='menu-link-wrap'>
-          <Link to="/using" currentPage={this.props.currentPage} className='menu-link'>Using</Link>
-        </span>
-        <span className='menu-link-wrap'>
-          <Link to="/base" currentPage={this.props.currentPage} className='menu-link'>Base</Link>
-        </span>
-        <div className='sub-menu'>
+        <div className='menu-wrap'>
           <span className='menu-link-wrap'>
-            <Link to="/base/grid" currentPage={this.props.currentPage} className='sub-menu-link'>Grid</Link>
+            <Link to="/principles" currentPage={this.props.currentPage} className='menu-link'>Principles</Link>
           </span>
           <span className='menu-link-wrap'>
-            <Link to="/base/typography" currentPage={this.props.currentPage} className='sub-menu-link'>Typography</Link>
+            <Link to="/using" currentPage={this.props.currentPage} className='menu-link'>Using</Link>
           </span>
           <span className='menu-link-wrap'>
-            <Link to="/base/colors" currentPage={this.props.currentPage} className='sub-menu-link'>Colors</Link>
+            <Link to="/base" currentPage={this.props.currentPage} className='menu-link'>Base</Link>
+          </span>
+          <div className='sub-menu'>
+            <span className='menu-link-wrap'>
+              <Link to="/base/grid" currentPage={this.props.currentPage} className='sub-menu-link'>Grid</Link>
+            </span>
+            <span className='menu-link-wrap'>
+              <Link to="/base/typography" currentPage={this.props.currentPage} className='sub-menu-link'>Typography</Link>
+            </span>
+            <span className='menu-link-wrap'>
+              <Link to="/base/colors" currentPage={this.props.currentPage} className='sub-menu-link'>Colors</Link>
+            </span>
+          </div>
+          <span className='menu-link-wrap'>
+            <Link to="/components" currentPage={this.props.currentPage} className='menu-link'>Components</Link>
+          </span>
+          <div className='sub-menu'>
+            <span className='menu-link-wrap'>
+              <Link to="/components/buttons" currentPage={this.props.currentPage} className='sub-menu-link'>Buttons</Link>
+            </span>
+            <span className='menu-link-wrap'>
+              <Link to="/components/fields" currentPage={this.props.currentPage} className='sub-menu-link'>Fields</Link>
+            </span>
+            <span className='menu-link-wrap'>
+              <Link to="/components/controls" currentPage={this.props.currentPage} className='sub-menu-link'>Controls</Link>
+            </span>
+            <span className='menu-link-wrap'>
+              <Link to="/components/additional" currentPage={this.props.currentPage} className='sub-menu-link'>Additional</Link>
+            </span>
+          </div>
+          <span className='menu-link-wrap'>
+            <Link to="/additional" currentPage={this.props.currentPage} className='menu-link'>Additional</Link>
+          </span>
+          <span className='menu-link-wrap'>
+            <Link to="/themes" currentPage={this.props.currentPage} className='menu-link'>Themes</Link>
           </span>
         </div>
-        <span className='menu-link-wrap'>
-          <Link to="/components" currentPage={this.props.currentPage} className='menu-link'>Components</Link>
-        </span>
-        <div className='sub-menu'>
-          <span className='menu-link-wrap'>
-            <Link to="/components/buttons" currentPage={this.props.currentPage} className='sub-menu-link'>Buttons</Link>
-          </span>
-          <span className='menu-link-wrap'>
-            <Link to="/components/fields" currentPage={this.props.currentPage} className='sub-menu-link'>Fields</Link>
-          </span>
-          <span className='menu-link-wrap'>
-            <Link to="/components/controls" currentPage={this.props.currentPage} className='sub-menu-link'>Controls</Link>
-          </span>
-          <span className='menu-link-wrap'>
-            <Link to="/components/additional" currentPage={this.props.currentPage} className='sub-menu-link'>Additional</Link>
-          </span>
-        </div>
-        <span className='menu-link-wrap'>
-          <Link to="/additional" currentPage={this.props.currentPage} className='menu-link'>Additional</Link>
-        </span>
-        <span className='menu-link-wrap'>
-          <Link to="/themes" currentPage={this.props.currentPage} className='menu-link'>Themes</Link>
-        </span>
       </div>
     )
   }
