fba01d6039d65c00096fcf77fa54914d1bc1a0a3
Pavel Frolov <pfrlvgg@gmail.com>
2017-08-28 22:30:59 -0700

    up
    Start added Typography section & update content styles

--- src/containers/Typography.js
+++ src/containers/Typography.js
@@ -1,11 +1,78 @@
-import React, { Component } from 'react'
+import React from 'react'
 
-class Typography extends Component {
-  render() {
-    return (
-      <span className='sub-headline'>Typography</span>
-    )
-  }
-}
-
-export default Typography
+export default () => (
+  <div>
+    <span className="sub-headline">Typography</span>
+    <div className="content-body">
+      <h1>Love all, trust a few, do wrong to none.</h1>
+      <h2>Can one desire too much of a good thing?</h2>
+      <h3>That show, contain, and nourish all the world.</h3>
+      <p className="big">
+        O, reason not the need: our basest beggars 
+        Are in the poorest thing superfluous:
+        Allow not nature more than nature needs,
+        Man's life's as cheap as beast's: thou art a lady;
+        If only to go warm were gorgeous,
+        Why, nature needs not what thou gorgeous wear'st,
+        Which scarcely keeps thee warm. But, for true need,-
+        You heavens, give me that patience, patience I need!
+        You see me here, you gods, a poor old man,
+        As full of grief as age; wretched in both!
+        If it be you that stir these daughters' hearts
+        Against their father, fool me not so much
+        To bear it tamely; touch me with noble anger,
+        And let not women's weapons, water-drops, 
+        Stain my man's cheeks! 
+      </p>
+      <p>
+        Nay, if thou talk of reason, then be mute;
+        For with good reason I can thee confute.
+        If they, which first by nature's sacred law,
+        Do owe to me the tribute of their lives;
+        If they to whom I always have been kind,
+        And bountiful beyond comparison;
+        If they, for whom I have undone myself,
+        And brought my age unto this extreme want,
+        Do now reject, contemn, despise, abhor me,
+        What reason moveth thee to sorrow for me? 
+      </p>
+      <p className="small">
+        Know that we have divided 
+        In three our kingdom: and 'tis our fast intent
+        To shake all cares and business from our age;
+        Conferring them on younger strengths, while we
+        Unburthen'd crawl toward death. Our son of Cornwall,
+        And you, our no less loving son of Albany,
+        We have this hour a constant will to publish 
+        Our daughters' several dowers, that future strife
+        May be prevented now. The princes, France and Burgundy,
+        Great rivals in our youngest daughter's love,
+        Long in our court have made their amorous sojourn,
+        And here are to be answer'd. Tell me, my daughters,-
+        Since now we will divest us both of rule,
+        Interest of territory, cares of state,-
+        Which of you shall we say doth love us most?
+        That we our largest bounty may extend 
+        Where nature doth with merit challenge. Goneril,
+        Our eldest-born, speak first. 
+      </p>
+      <p className="caption">
+        Dear Gonorill, kind Ragan, sweet Cordella,
+        Ye flourishing branches of a kingly stock,
+        Sprung from a tree that once did flourish green,
+        Whose blossoms now are nipped with winter's frost,
+        And pale grim death doth wait upon my steps,
+        And summons me unto his next assizes.
+        Therefore, dear daughters, as ye tender the safety
+        Of him that was the cause of your first being,
+        Resolve a doubt which much molests my mind,
+        Which of you three to me would prove most kind;
+        Which loves me most, and which at my request
+        Will soonest yield unto their father's hest. 
+      </p>
+      <blockquote>
+        The course of true love never did run smooth.
+      </blockquote>
+    </div>
+  </div>
+)
